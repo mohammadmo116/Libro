475b688e3c6c6a3bcd870ee92c847ace5f6757e0
MohammadMousa116 <momousa.ce@gmail.com>
2023-05-23 11:24:50 +0300

    LIB23 create book, author, and authorBook table
    add genre prop

--- Libro.Infrastructure/Migrations/20230523082440_Book_Genre_prop.Designer.cs
+++ Libro.Infrastructure/Migrations/20230523082440_Book_Genre_prop.Designer.cs
@@ -4,6 +4,7 @@ using Libro.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Libro.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230523082440_Book_Genre_prop")]
+    partial class Book_Genre_prop
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
