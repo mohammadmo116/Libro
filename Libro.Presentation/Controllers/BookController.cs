bd5552c4db76e7e9a9a361a6566a0c73858cad21
MohammadMousa116 <momousa.ce@gmail.com>
2023-05-23 12:27:23 +0300

    LIB-24 Create Book Repository LIB-26 create end-point (controller)
    LIB-24 Fix the implementation for the search method
    Lib-26 implement the controller

---
+++ Libro.Presentation/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using Libro.Application.Books.Queries;
+using Libro.Application.Roles.Commands;
+using Libro.Domain.Entities;
+using Libro.Presentation.Dtos.Role;
+using Libro.Presentation.Dtos.User;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.Presentation.Controllers
+{
+    [ApiController]
+    [Route("Book")]
+    public class BookController : ControllerBase
+    {   
+        private readonly IMediator _mediator;
+
+        public BookController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("Books", Name = "search")]
+        public async Task<ActionResult<List<string>>> Search(string? Title, string? AuthorName, string? Genre)
+        {
+            
+            var query = new GetSearchedBooksQuery(Title, AuthorName, Genre);
+            var Result = await _mediator.Send(query);
+            return Ok(Result);
+        }
+    }
+}
