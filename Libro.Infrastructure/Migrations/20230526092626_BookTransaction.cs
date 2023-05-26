73efc44b2f4ac7f1714d6c7403f4e72f9153d6aa
MohammadMousa116 <momousa.ce@gmail.com>
2023-05-26 13:42:25 +0300

    LIB-57 Create BookTransaction Repository
    BookTransaction Repository has ReserveBook method  which allow patrons to reserve books

---
+++ Libro.Infrastructure/Migrations/20230526092626_BookTransaction.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Libro.Infrastructure.Migrations
+{
+    public partial class BookTransaction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookUsers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    BorrowedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookUsers_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookUsers_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookUsers_BookId",
+                table: "BookUsers",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookUsers_UserId",
+                table: "BookUsers",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookUsers");
+        }
+    }
+}
