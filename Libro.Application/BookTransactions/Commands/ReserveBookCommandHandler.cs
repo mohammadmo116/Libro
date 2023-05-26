b0b9268562d32b0dc112559dd836499bd6204a6c
MohammadMousa116 <momousa.ce@gmail.com>
2023-05-26 14:05:41 +0300

    LIB-58 Create CQRS for reserving books
    ReserveBookCommand
    
    ReserveBookCommandHandler

---
+++ Libro.Application/BookTransactions/Commands/ReserveBookCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using Libro.Application.Interfaces;
+using Libro.Domain.Entities;
+using Libro.Domain.Exceptions;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.Application.BookTransactions.Commands
+{
+    public sealed class ReserveBookCommandHandler : IRequestHandler<ReserveBookCommand>
+    {
+        private readonly ILogger<BookTransaction> _logger;
+        private readonly IBookTransactionRepository _bookTransactionRepository;
+
+       public ReserveBookCommandHandler(ILogger<BookTransaction> logger,
+           IBookTransactionRepository bookTransactionRepository) {
+            _logger = logger;
+            _bookTransactionRepository = bookTransactionRepository;
+
+        }
+        public async Task Handle(ReserveBookCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+               await _bookTransactionRepository.ReserveBook(request.bookTransaction);
+            }
+            catch (CustomNotFoundException e)
+            {
+                _logger.LogInformation($"CustomNotFoundException message:{e.Message}");
+                _logger.LogInformation($"bookId : {request.bookTransaction.BookId}, userId : {request.bookTransaction.UserId}");
+                throw e;
+            }
+            catch (BookIsNotAvailableException e)
+            {
+                _logger.LogInformation($"BookIsNotAvailableException message:{e.Message}");
+                _logger.LogInformation($"bookId : {request.bookTransaction.BookId}, userId : {request.bookTransaction.UserId}");
+                throw e;
+            }
+        }
+    }
+
+}
