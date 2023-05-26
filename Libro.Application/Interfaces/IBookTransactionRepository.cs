c4e9509574bbc24e1e58253e965aadc165570a8b
MohammadMousa116 <momousa.ce@gmail.com>
2023-05-26 13:48:49 +0300

    LIB-57 Create BookTransaction Repository
    (updated) adding the interface and the scope

---
+++ Libro.Application/Interfaces/IBookTransactionRepository.cs
@@ -0,0 +1,9 @@
+﻿using Libro.Domain.Entities;
+
+namespace Libro.Application.Interfaces
+{
+    public interface IBookTransactionRepository
+    {
+        Task ReserveBook(BookTransaction bookTransaction);
+    }
+}
