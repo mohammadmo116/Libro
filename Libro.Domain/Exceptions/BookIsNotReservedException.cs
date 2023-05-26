d25d98ead8ecb06de67c9f330fda06fc5d38bf53
MohammadMousa116 <momousa.ce@gmail.com>
2023-05-26 15:22:27 +0300

    LIB-60 add CheckOut method in the repository
    add CheckOut method in the BookTransaction repository
    which change the  bookTransaction state to borrowed after checking if the book is found and already reserved

---
+++ Libro.Domain/Exceptions/BookIsNotReservedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.Domain.Exceptions
+{
+    public class BookIsNotReservedException :Exception
+
+    { 
+        public BookIsNotReservedException(string title) : base($"the Requested book {title} is not reserved, please reserve the book first")
+        { 
+        }
+    }
+}
