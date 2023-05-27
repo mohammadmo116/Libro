4951bf70e22023ace3530a2668e59a0383fd399e
MohammadMousa116 <momousa.ce@gmail.com>
2023-05-27 20:12:38 +0300

    LIB-69 add CQRS
    TrackDueDateQuery and TrackDueDateQueryHandler

---
+++ Libro.Application/BookTransactions/Queiries/TrackDueDateQuery.cs
@@ -0,0 +1,8 @@
+﻿using Libro.Domain.Entities;
+using MediatR;
+
+namespace Libro.Application.BookTransactions.Queiries
+{
+    public sealed record TrackDueDateQuery:IRequest<List<BookTransaction>>;
+    
+}
