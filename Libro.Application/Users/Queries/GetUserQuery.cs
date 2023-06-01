36f9a46a714d938099af83b9754a47c21a3ca22d
MohammadMousa116 <momousa.ce@gmail.com>
2023-06-01 18:42:00 +0300

    LIB-83 add CQRS and test
    add GetUserQuery and GetUserQueryHandle and implent the testing

---
+++ Libro.Application/Users/Queries/GetUserQuery.cs
@@ -0,0 +1,12 @@
+﻿using Libro.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.Application.Users.Queries
+{
+    public sealed record class GetUserQuery(Guid UserId):IRequest<User>;
+}
---
+++ Libro.Application/Users/Queries/GetUserQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Libro.Application.Interfaces;
+using Libro.Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Libro.Application.Users.Queries
+{
+    public class GetUserQueryHandler : IRequestHandler<GetUserQuery, User>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILogger<GetUserQueryHandler> _logger;
+
+        public GetUserQueryHandler(
+            ILogger<GetUserQueryHandler> logger,
+            IUserRepository userRepository     
+            ) 
+        {
+            _logger = logger;
+            _userRepository = userRepository;
+          
+        }
+        public Task<User> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
