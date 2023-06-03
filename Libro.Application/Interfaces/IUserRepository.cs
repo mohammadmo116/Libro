61e18f2b5fc9a85899b3dcafc157ccc01d7a0c53
MohammadMousa116 <momousa.ce@gmail.com>
2023-06-03 17:38:45 +0300

    LIB-104 add end-point for update Patron  user

--- Libro.Application/Interfaces/IUserRepository.cs
+++ Libro.Application/Interfaces/IUserRepository.cs
@@ -10,8 +10,11 @@ namespace Libro.Application.Interfaces
         Task<User> RegisterUserAsync(User user);
         void UpdateUser(User user);
         Task<bool> EmailIsUniqueAsync(string Email);
+        Task<bool> EmailIsUniqueForUpdateAsync(Guid UserId, string Email);
         Task<bool> UserNameIsUniqueAsync(string UserName);
+        Task<bool> UserNameIsUniqueForUpdateAsync(Guid UserId, string UserName);
         Task<bool> PhoneNumberIsUniqueAsync(string PhoneNumber);
+        Task<bool> PhoneNumberIsUniqueForUpdateAsync(Guid UserId, string PhoneNumber);
         Task<User> GetUserAsync(Guid UserId);
         Task<User> GetUserWtithRolesAsync(Guid UserId);
         Task<List<BookTransaction>> GetBorrowingHistoryAsync(Guid UserId, int PageNumber, int Count);
