00419f17409a12cb785cfb0a15b922c6a7dfffdc
MohammadMousa116 <momousa.ce@gmail.com>
2023-06-06 16:07:12 +0300

    LIB-143 Improve Book Search
    improving the seach method on the books in the book repository by filtering IQueryable instead of list

--- Libro.Infrastructure/Repositories/BookRepository.cs
+++ Libro.Infrastructure/Repositories/BookRepository.cs
@@ -49,31 +49,45 @@ namespace Libro.Infrastructure.Repositories
         {
             _context.Books.Remove(book);
         }
+        public async Task<(List<Book>,int)> GetSearchedBooksAsync(string? Title, string? AuthorName, string? Genre, int PageNumber, int Count) {
+            var Books = _context.Books.Where(a => a.IsAvailable == true);
+            if (Title is not null)
+                Books = GetBooksByTitleAsync(Books, Title);
 
-        public async Task<List<Book>> GetBooksByAuthorNameAsync(List<Book> Books, string? AuthorName)
+            if (Genre is not null)
+                Books = GetBooksByGenreAsync(Books, Genre);
+
+            if (AuthorName is not null)
+                Books = await GetBooksByAuthorNameAsync(Books, AuthorName);
+
+            var booksCount = await Books.CountAsync();
+            var SearchedBooks = await Books.Skip(PageNumber * Count).Take(Count).ToListAsync();
+            var NumberOfPages = 1;
+            if (booksCount > 0)
+                NumberOfPages = (int)Math.Ceiling((double)booksCount / Count);
+           
+            return (SearchedBooks, NumberOfPages);
+            
+            
+        }
+        private async Task<IQueryable<Book>> GetBooksByAuthorNameAsync(IQueryable<Book>? Books, string AuthorName)
         {
+           
             var AuthorIds = await _context.Authors.Where(b => b.Name.Contains(AuthorName)).Select(a => a.Id).ToListAsync();
             var BookIds = await _context.AuthorBooks.Where(a => AuthorIds.Contains(a.AuthorId)).Select(a => a.BookId).ToListAsync();
-            Books = await _context.Books.Where(b => BookIds.Contains(b.Id)).ToListAsync();
-            return Books;
+            return Books.Where(b => BookIds.Contains(b.Id));
+          
         }
 
-        public async Task<List<Book>> GetBooksByGenreAsync(List<Book>? Books, string Genre)
+        private IQueryable<Book> GetBooksByGenreAsync(IQueryable<Book> Books, string Genre)
         {
-            if (Books is null)
-                Books = await _context.Books.Where(b => b.Genre.Contains(Genre)).ToListAsync();
-            else
-                Books = Books.Where(b => b.Genre.Contains(Genre)).ToList();
-            return Books;
+             return Books.Where(b => b.Genre.Contains(Genre));
         }
-
-        public async Task<List<Book>> GetBooksByTitleAsync(List<Book>? Books, string Title)
+        private IQueryable<Book> GetBooksByTitleAsync(IQueryable<Book> Books, string Title)
         {
-            if (Books is null)
-                Books = await _context.Books.Where(b => b.Title.Contains(Title)).ToListAsync();
-            else
-                Books = Books.Where(b => b.Title.Contains(Title)).ToList();
-            return Books;
+
+            return Books.Where(b => b.Title.Contains(Title));
+             
         }
         public void MakeBookNotAvailable(Book book)
         {
@@ -85,6 +99,7 @@ namespace Libro.Infrastructure.Repositories
             book.IsAvailable = true;
             _context.Books.Update(book);
         }
-    
+
+     
     }
 }
