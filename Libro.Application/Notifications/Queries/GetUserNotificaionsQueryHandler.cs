16fb26e256fc3a2667ea9e26b5f0a561664c4340
MohammadMousa116 <momousa.ce@gmail.com>
2023-06-10 02:21:11 +0300

    LIB-151 As a user I want to see my notifications with pagination
    add GetNotifications method to Notification repository
    
    add GetUserNotificaionsQuery and GetUserNotificaionsQueryHandler
    
    Implement GetUserNotificaionsQueryHandlerTest for GetUserNotificaionsQueryHandler
    
    add end-point  GetUserNotifications with route Get /Notification

---
+++ Libro.Application/Notifications/Queries/GetUserNotificaionsQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using Libro.Application.Interfaces;
+using Libro.Application.Notifications.Commands;
+using Libro.Domain.Entities;
+using Libro.Infrastructure.Repositories;
+using Libro.Infrastructure;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Libro.Domain.Exceptions;
+
+namespace Libro.Application.Notifications.Queries
+{
+    public sealed class GetUserNotificaionsQueryHandler:IRequestHandler<GetUserNotificaionsQuery,(List<Notification>,int)>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly INotificationRepository _notificationRepository;
+        private readonly ILogger<GetUserNotificaionsQueryHandler> _logger;
+ 
+        public GetUserNotificaionsQueryHandler(
+           INotificationRepository notificationRepository,
+           IUserRepository userRepository,
+           ILogger<GetUserNotificaionsQueryHandler> logger
+           )
+        {
+            _notificationRepository = notificationRepository;
+            _userRepository = userRepository;
+            _logger = logger;
+        }
+
+        public async Task<(List<Notification>,int)> Handle(GetUserNotificaionsQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserAsync(request.UserId);
+            if (user is null)
+            {
+                _logger.LogInformation("CustomNotFoundException (User)");
+                throw new CustomNotFoundException("User");
+            }
+           return await _notificationRepository.GetNotifications(request.UserId, request.PageNumbr, request.Count);
+        }
+    }
+}
---
+++ Libro.Infrastructure/Migrations/20230609225202_AddIsReadToNotificationTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Libro.Infrastructure.Migrations
+{
+    public partial class AddIsReadToNotificationTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRead",
+                table: "Notifications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRead",
+                table: "Notifications");
+        }
+    }
+}
