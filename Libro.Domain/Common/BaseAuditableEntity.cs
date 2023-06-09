e5ee8d6f4d8978173aa84ce5d5e172699ba7a038
MohammadMousa116 <momousa.ce@gmail.com>
2023-06-09 19:16:38 +0300

    LIB-148 add notification table and run migration
    add Notification table which includes
    
    Message
    
    UserId

--- Libro.Domain/Common/BaseAuditableEntity.cs
+++ Libro.Domain/Common/BaseAuditableEntity.cs
@@ -1,8 +1,11 @@
-﻿namespace Libro.Domain.Common
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Libro.Domain.Common
 {
     public abstract class BaseAuditableEntity : BaseEntity
     {
-
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Id { get; set; }
         public DateTime Created { get; set; }
 
         public string? CreatedBy { get; set; }
--- Libro.Domain/Entities/User.cs
+++ Libro.Domain/Entities/User.cs
@@ -28,5 +28,6 @@ namespace Libro.Domain.Entities
         public List<BookTransaction>? BookTransactions { get; set; } = new();
         public List<ReadingList>? ReadingLists { get; set; }
         public List<BookReview>? Reviews { get; set; }
+        public List<Notification>? Notifications { get; set; }
     }
 }
