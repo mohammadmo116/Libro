18b03bb63f076a29046677d0a1b846bbfba11fd1
MohammadMousa116 <momousa.ce@gmail.com>
2023-06-19 18:32:37 +0300

    LIB-168 Author Controller Test

---
+++ Libro.Presentation/Validators/AuthorDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Libro.Presentation.Dtos.Author;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.Presentation.Validators
+{
+    public class AuthorDtoValidator : AbstractValidator<AuthorDto>
+    {
+        public AuthorDtoValidator()
+        {
+            RuleFor(a => a.DateOfBirth).LessThan(DateTime.UtcNow);
+        }
+    }
+}
